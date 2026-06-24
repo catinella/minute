a2602ada405be2632e195ef586f3347bd5f5e070
scatinella <catinella@yahoo.com>
2026-06-24 16:19:57 +0200

    Code added to test --vebose option

--- test/test.c
+++ test/test.c
@@ -1,18 +1,26 @@
 #include <minute.h>
 
 TEST (myFunction1, testType1) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		printf("This is a verbose log.... bla bla bla\n");
 	ASSERT_EQ(1, 1);
 	ASSERT_EQ(1, 1);
 }
 TEST (myFunction1, testType2) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		printf("This is a verbose log.... bla bla bla\n");
 	ASSERT_EQ(1, 1);
 	ASSERT_EQ(1, 1);
 }
 TEST (myFunction2, testType1) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		printf("This is a verbose log.... bla bla bla\n");
 	ASSERT_EQ(1, 1);
 	ASSERT_EQ(1, 1);
 }
 TEST (myFunction2, testType2) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		printf("This is a verbose log.... bla bla bla\n");
 	ASSERT_EQ(1, 1);
 }
 
