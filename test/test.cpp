549a441e4fdfc419417e6d8ee0d78de947b301b1
Silvano Catinella <silvano.catinella@fimer.com>
2026-06-25 08:28:15 +0200

    backup version

---
+++ test/test.cpp
@@ -0,0 +1,45 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//
+//                                            __  __ _             _   _     _____    
+//                                           |  \/  (_)_ __       | | | |   |_   _|__ 
+//                                           | |\/| | | '_ \ _____| | | |_____| |/ _ \
+//                                           | |  | | | | | |_____| |_| |_____| |  __/
+//                                           |_|  |_|_|_| |_|      \___/      |_|\___|
+//                                                    Minimal Unit Test support
+//
+// File:      test.cpp
+//
+// Author:    Silvano Catinella
+//
+// Description
+//	It is a simple unit-test example in C++
+//
+// Editor parameters: 128 cols, ts=6
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <minute.h>
+
+TEST (myFunction1, testType1) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		cout << "This is a verbose log.... bla bla bla" << endl;
+	ASSERT_EQ(1, 1);
+	ASSERT_EQ(1, 1);
+}
+TEST (myFunction1, testType2) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		cout << "This is a verbose log.... bla bla bla" << endl;
+	ASSERT_EQ(1, 1);
+	ASSERT_EQ(1, 1);
+}
+TEST (myFunction2, testType1) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		cout << "This is a verbose log.... bla bla bla" << endl;
+	ASSERT_EQ(1, 1);
+	ASSERT_EQ(1, 1);
+}
+TEST (myFunction2, testType2) {
+	if (fileArgumentsDb_get("verbose", NULL))
+		cout << "This is a verbose log.... bla bla bla" << endl;
+	ASSERT_EQ(1, 1);
+}
+
+#include "./test_main.c"
